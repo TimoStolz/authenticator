8ae0cdbdef0e0e5d4d7437bd9233118fd0159e6d
Timo Stolz <timo.stolz@gerade-deshalb.org>
2018-11-02 06:02:20 +0100

    allow username in registration, allow login with username or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,29 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
+
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
+  attr_accessor :login
+
+  def login
+    @login || username || email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    # tutorial for logins with username or email
+    # https://github.com/plataformatec/devise/wiki/how-to:-allow-users-to-sign-in-using-their-username-or-email-address
+    conditions = warden_conditions.dup
+    conditions[:email]&.downcase!
+    conditions[:username]&.downcase!
+    conditions[:login]&.downcase!
+
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value', { value: login }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
