75a9852dc39e5b140d1bdef0f7e054fe61ce9089
Timo Stolz <timo.stolz@gerade-deshalb.org>
2018-11-02 13:34:21 +0100

    drop CanCan related code from ApiController

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,3 @@
 class ApiController < ApplicationController
   include IsApi
-
-  rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { render json: { error: exception.message }, status: :forbidden }
-      format.html { redirect_to main_app.root_url, notice: exception.message }
-    end
-  end
-
 end
